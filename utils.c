bf0d80d37c8ce4e643006054c3b363e1b1d33268
ahmetkca <ahmet.karapinar@ontariotechu.net>
2022-02-16 01:39:44 -0500

    fixed malloc top size core dump bug.

--- utils.c
+++ utils.c
@@ -5,16 +5,19 @@
 #include <string.h>
 #include "utils.h"
 
+#define BUFLEN 256 
+
 // wrapper funtion around printf to output to console with colors
 void color_printf(const char *color, const bool newline, const char *format,  ...) {
-    va_list args;
-    va_start(args, format);
-    char *temp = malloc(sizeof(char) * 256);
+    char temp[BUFLEN*8] = {0};
     strcat(temp, color);
     strcat(temp, format);
     strcat(temp, ANSI_COLOR_RESET);
-    if (newline)
+    if (newline == true) {
         strcat(temp, "\n");
+    }
+    va_list args;
+    va_start(args, format);
     vprintf(temp,  args);
     va_end(args);
 }
