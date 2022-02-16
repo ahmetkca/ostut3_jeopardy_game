82ee0f9c9eea75aeda6dc340de0fa36dc91ecd70
ahmetkca <ahmet.karapinar@ontariotechu.net>
2022-02-15 22:21:06 -0500

    changed standard printf with color_printf. Fixed tokenize function

--- jeopardy.c
+++ jeopardy.c
@@ -13,49 +13,25 @@
 #include "questions.h"
 #include "players.h"
 #include "jeopardy.h"
+#include "utils.h"
 
 // Put macros or constants here using #define
 #define BUFFER_LEN 256
 #define NUM_PLAYERS 4
 
-// Put global environment variables here
-
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
-
-// wrapper funtion around printf to output to console with colors
-void color_printf(const char *color, const bool *newline, const char *format,  ...) {
-    va_list args;
-    va_start(args, format);
-    char *temp = malloc(1);
-    strcat(temp, color);
-    strcat(temp, format);
-    strcat(temp, ANSI_COLOR_RESET);
-    if (newline)
-        strcat(temp, "\n");
-    vprintf(temp,  args);
-    va_end(args);
-}
-
 // Compare function used to sort players array by score 
-int cmpfunc(const void * a, const void * b){
+int compare_two_players_by_score(const void * a, const void * b){
     player *playerA = (player *)a; 
     player *playerB = (player *)b;
     return (playerB->score - playerA->score);
 }
-// Put global environment variables here
 
 // Processes the answer from the user containing what is or who is and tokenizes it to retrieve the answer.
-void tokenize(char *input, char **tokens) {
+void tokenize(char *input, char tokens[3][MAX_LEN]) {
     char delim[2];
     delim[0] = ' ';
     delim[1] = 0;
-    char *ans = malloc(1);
+    char ans[MAX_LEN] = {0};
     char *token = strtok(input, delim);
     int i = 0;
     while (token != NULL) {
@@ -72,17 +48,19 @@ void tokenize(char *input, char **tokens) {
     for (int i = 0; i < strlen(ans) - 1; i++) {
         tmp_ans[i] = ans[i];
     }
-    strcpy(token[2], tmp_ans);
+    strcpy(tokens[2], tmp_ans);
 }
+
+
 // Displays the game results for each player, their name and final score, ranked from first to last place
 void show_results(player *players, int num_players){
     
     //Quick sort players score
-    qsort(players,4,sizeof(player),cmpfunc);
+    qsort(players, num_players, sizeof(player), compare_two_players_by_score);
 
     //Print player scores by ranking
     for(int i = 0; i < NUM_PLAYERS; i++){
-        printf("Rank: %d\t Name: %s\tScore:%d\n", i+1, players[i].name, players[i].score);
+        color_printf(ANSI_COLOR_GREEN ,true, "Rank: %d\t Name: %s\tScore:%d", i+1, players[i].name, players[i].score);
     }
 }
 
@@ -95,11 +73,12 @@ int main(int argc, char *argv[])
     char buffer[BUFFER_LEN] = { 0 };
 
     // Display the game introduction and initialize the questions
+    set_questions_file_path("questions.txt");
     initialize_game();
 
     // Prompt for players names
-    printf(ANSI_COLOR_CYAN "Welcome to Jeopardy" ANSI_COLOR_RESET "\n");
-    printf(ANSI_COLOR_RED"Please enter the players names: "ANSI_COLOR_RESET"\n");
+    color_printf(ANSI_COLOR_CYAN, true, "Welcome to Jeopardy");
+    color_printf(ANSI_COLOR_RED, true, "Please enter the players names: ");
     
     // initialize each of the players in the array
     for(int i =0; i < NUM_PLAYERS; i++){
@@ -112,8 +91,8 @@ int main(int argc, char *argv[])
         
         //variables
         int questions = NUM_QUESTIONS;
-        char *category;
-        int value;
+        char *category = malloc(sizeof(char)*BUFFER_LEN);
+        int value = 0;
         char response[BUFFER_LEN];
         char token[3][BUFFER_LEN] = {{0}};
 
@@ -127,12 +106,12 @@ int main(int argc, char *argv[])
                 
                 do{
                     // Pick Category
-                    printf("It is %s turn. \nPlease select the category: \n",players[i].name);
+                    color_printf(ANSI_COLOR_MAGENTA, true, "It is %s turn. \nPlease select the category: ",players[i].name);
                     scanf("%s", category);
 
                     // Pick value of question
-                    printf(" The value of question: ");
-                    scanf("%d\n", value);
+                    color_printf(ANSI_COLOR_BLUE, false, " The value of question: ");
+                    scanf("%d\n", &value);
                     
                     }while(already_answered(category,value));
                     
@@ -140,7 +119,7 @@ int main(int argc, char *argv[])
                     display_question(category, value);
                     scanf("%s", response);
 
-                    tokenize(response,token);
+                    tokenize(response, token);
                     
                     if(valid_answer(category,value,token[2]))
                     {
