76323636e9c68e3e1c954614d34bdd3b5eeaaaa7
ahmetkca <ahmet.karapinar@ontariotechu.net>
2022-02-16 15:21:19 -0500

    added function to get random ansi color to use it in color_printf func

--- utils.c
+++ utils.c
@@ -3,9 +3,12 @@
 #include <stdbool.h>
 #include <stdarg.h>
 #include <string.h>
+#include <time.h>
 #include "utils.h"
 
-#define BUFLEN 256 
+#define BUFLEN 256
+#define MAX_RGB_VAL 255
+
 
 // wrapper funtion around printf to output to console with colors
 void color_printf(const char *color, const bool newline, const char *format,  ...) {
@@ -20,4 +23,14 @@ void color_printf(const char *color, const bool newline, const char *format,  ..
     va_start(args, format);
     vprintf(temp,  args);
     va_end(args);
+}
+
+// random ansi color generator
+char * get_random_ansi_color(void) {
+    int rand_rgb_val = rand() % 255;
+    char * random_ansi_color;
+    if (random_ansi_color != NULL)
+        random_ansi_color = malloc((64 * sizeof(char))+1);
+    snprintf(random_ansi_color, 64 - 1, "\x1b[38;5;%dm", rand_rgb_val);
+    return random_ansi_color;
 }
